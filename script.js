84f089c85a0937f1529d2028aa780d5c2b44919d
Brenolegalzin <145219656+Brenolegalzin@users.noreply.github.com>
2024-01-11 22:13:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -42,15 +42,29 @@ function gere(){
   resposta = resposta+coisas[coisas1];
   document.getElementById("result").innerText = resposta;
   if(trueOrFalse1==true){
-      resposta.replace(["0","1","2","3","4","5","6","7","8","9"], "");
+      resposta.replace("0", "");
+      resposta.replace("1", "");
+      resposta.replace("2", "");
+      resposta.replace("3", "");
+      resposta.replace("4", "");
+      resposta.replace("5", "");
+      resposta.replace("6", "");
+      resposta.replace("7", "");
+      resposta.replace("8", "");
+      resposta.replace("9", "");
       document.getElementById("result").innerText = resposta;
   }
   if(trueOrFalse3==true){
-      resposta.replace([".","!","*","+"], "");
+      resposta.replace(".", "");
+      resposta.replace("+", "");
+      resposta.replace("!", "");
+      resposta.replace("*", "");
       document.getElementById("result").innerText = resposta;
   }
   if(trueOrFalse2==true){
-      resposta.replace(["a","b","c"], "");
+      resposta.replace("a", "");
+      resposta.replace("b", "");
+      resposta.replace("c", "");
       document.getElementById("result").innerText = resposta;
   }
 }
